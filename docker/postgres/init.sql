bded4693fb3a4d459de40ad80b4041f4ff133cc6
Gleb Tcivie <gleb@tcivie.com>
2024-07-05 11:45:20 +0300

    Added 2 tables for neighbour info

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -25,5 +25,30 @@ CREATE TABLE IF NOT EXISTS client_details
     short_name     VARCHAR,
     long_name      VARCHAR,
     hardware_model VARCHAR,
-    role           VARCHAR
+    role        VARCHAR,
+    mqtt_status VARCHAR default 'none'
 );
+
+CREATE TABLE IF NOT EXISTS node_graph
+(
+    node_id                 VARCHAR PRIMARY KEY,
+    last_sent_by_node_id    VARCHAR,
+    last_sent_at            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    broadcast_interval_secs INTEGER,
+    FOREIGN KEY (node_id) REFERENCES client_details (node_id)
+);
+
+CREATE TABLE IF NOT EXISTS node_neighbors
+(
+    id          SERIAL PRIMARY KEY,
+    node_id     VARCHAR,
+    neighbor_id VARCHAR,
+    snr         FLOAT,
+    FOREIGN KEY (node_id) REFERENCES client_details (node_id),
+    FOREIGN KEY (neighbor_id) REFERENCES node_graph (node_id),
+    UNIQUE (node_id, neighbor_id)
+);
+
+CREATE INDEX idx_node_neighbors_node_id ON node_neighbors (node_id);
+CREATE INDEX idx_node_neighbors_neighbor_id ON node_neighbors (neighbor_id);
+CREATE UNIQUE INDEX idx_unique_node_neighbor ON node_neighbors (node_id, neighbor_id);
